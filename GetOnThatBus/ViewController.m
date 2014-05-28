3ce5adb2203c961fdd16e74b0ebce03f7277de2d
initWit <figueras.robert@gmail.com>
2014-05-28 13:58:43 -0500

    MVP complete

--- GetOnThatBus/ViewController.m
+++ GetOnThatBus/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+
+@interface ViewController () <MKMapViewDelegate>
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -17,13 +20,48 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    NSURL *url = [NSURL URLWithString:@"https://s3.amazonaws.com/mobile-makers-lib/bus.json"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response,
+                                                                                                            NSData *data,
+                                                                                                            NSError *connectionError) {
+
+        NSDictionary *amazonDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+        NSArray *busStopsArray = [amazonDictionary objectForKey:@"row"];
+
+        for (NSDictionary *busDictionary in busStopsArray) {
+            NSString *latitude = [busDictionary objectForKey:@"latitude"];
+            NSString *longitude = [busDictionary objectForKey:@"longitude"];
+
+            double latConvertedToDouble = [latitude doubleValue];
+            double longConvertedToDouble = [longitude doubleValue];
+            MKPointAnnotation *busStopAnnotation = [[MKPointAnnotation alloc]init];
+            busStopAnnotation.coordinate = CLLocationCoordinate2DMake(latConvertedToDouble, longConvertedToDouble);
+            busStopAnnotation.title = [busDictionary objectForKey:@"cta_stop_name"];
+            busStopAnnotation.subtitle = [busDictionary objectForKey:@"routes"];
+
+            [self.mapView addAnnotation:busStopAnnotation];
+
+        }
+    }];
+
 }
 
-- (void)didReceiveMemoryWarning
+#pragma mark - AnnotationView Delegate Methods
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc]initWithAnnotation:annotation reuseIdentifier:nil];
+    pin.canShowCallout = YES;
+    return pin;
 }
+
+//- (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control
+//{
+//
+//
+//}
+
 
 @end
