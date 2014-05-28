5c71d476d3c2d914f044031b4fed62e07c219066
initWit <figueras.robert@gmail.com>
2014-05-28 13:07:08 -0500

    initial commit

---
+++ GetOnThatBus/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  GetOnThatBus
+//
+//  Created by Robert Figueras on 5/28/14.
+//
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    // Override point for customization after application launch.
+    return YES;
+}
+
+
+@end
---
+++ GetOnThatBus/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GetOnThatBus
+//
+//  Created by Robert Figueras on 5/28/14.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
